057b84822e880eb9a49767aa5444dabbbd823d2e
Martín Muñoz <103433945+martinm2001@users.noreply.github.com>
2022-05-16 14:22:54 -0300

    Update robo.ts

--- src/controllers/robo.ts
+++ src/controllers/robo.ts
@@ -1,13 +1,11 @@
 import mysql from "mysql";
-//const connection = require("./conec");
 
 const connection = mysql.createConnection({
-        host: "127.0.0.1",
-        user: "efaisa",
-        password: "1q2w3e4r5t6y",
-        database: "interfaz",
-      });
-
+  host: "127.0.0.1",
+  user: "efaisa",
+  password: "1q2w3e4r5t6y",
+  database: "interfaz",
+});
 
 const getRobo = (req: any, res: any) => {
   connection.query(
