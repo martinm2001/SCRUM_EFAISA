80b5a37f9e6f2ba404401355724ce087e1828425
Martín Muñoz <103433945+martinm2001@users.noreply.github.com>
2022-05-16 14:19:57 -0300

    Update controlacceso.ts

--- src/controllers/controlacceso.ts
+++ src/controllers/controlacceso.ts
@@ -1,15 +1,15 @@
-//Conexión con la base de datos via archivo conec
 import mysql from "mysql";
+
 const connection = mysql.createConnection({
-        host: "127.0.0.1",
-        user: "efaisa",
-        password: "1q2w3e4r5t6y",
-        database: "interfaz",
-      });
+  host: "127.0.0.1",
+  user: "efaisa",
+  password: "1q2w3e4r5t6y",
+  database: "interfaz",
+});
 
 const getControlAcceso = (req: any, res: any) => {
   connection.query(
-    `SELECT activado, nombreCA, URLCA FROM controlAcceso WHERE id = 1`,
+    `SELECT activado, nombreCA, URLCA FROM controlacceso WHERE id = 1`,
     function (err, result) {
       if (err) throw err;
       res.json(result[0]);
@@ -21,7 +21,7 @@ const getControlAcceso = (req: any, res: any) => {
 const putControlAcceso = (req: any, res: any) => {
   const { activado, nombreCA, URLCA } = req.body;
   connection.query(
-    `UPDATE controlAcceso SET activado = ${activado}, nombreCA = '${nombreCA}', URLCA = '${URLCA}' WHERE id=1`,
+    `UPDATE controlacceso SET activado = ${activado}, nombreCA = '${nombreCA}', URLCA = '${URLCA}' WHERE id=1`,
     function (err, result) {
       if (err) {
         res.json({ status: "Fail" });
