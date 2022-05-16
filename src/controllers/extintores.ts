9a7816da0cbb45cf4889ef310f78b37962e4d5e6
Martín Muñoz <103433945+martinm2001@users.noreply.github.com>
2022-05-16 10:14:33 -0300

    Update extintores.ts

--- src/controllers/extintores.ts
+++ src/controllers/extintores.ts
@@ -1,14 +1,13 @@
+//Conexión con la base de datos via archivo conec
 import mysql from "mysql";
-function conexion(){
 
-    const connection = mysql.createConnection({
+const connection = mysql.createConnection({
         host: "127.0.0.1",
         user: "efaisa",
         password: "1q2w3e4r5t6y",
         database: "interfaz",
       });
-      return connection;
-}
+
 
 const dayjs = require("dayjs");
 const isBetween = require("dayjs/plugin/isBetween");
