8f39c9f380fcf3c0bfca11878c4355aba9f51af3
Martín Muñoz <103433945+martinm2001@users.noreply.github.com>
2022-05-16 09:45:56 -0300

    Update eventos.ts

--- src/controllers/eventos.ts
+++ src/controllers/eventos.ts
@@ -1,5 +1,17 @@
 let eventosManuales: number[] = [];
 
+import mysql from "mysql";
+function conexion(){
+
+    const connection = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "efaisa",
+        password: "1q2w3e4r5t6y",
+        database: "interfaz",
+      });
+      return connection;
+}
+
 const getEventos = (req: any, res: any) => {
   if (req.query.id) {
     connection.query(
