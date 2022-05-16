2a7f133f2dbf5e9a62820ec289253cac48df338a
Martín Muñoz <103433945+martinm2001@users.noreply.github.com>
2022-05-16 10:45:43 -0300

    Update inout.ts

--- src/controllers/inout.ts
+++ src/controllers/inout.ts
@@ -1,11 +1,13 @@
 import mysql from "mysql";
+const connection = require("./conec");
 
-const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "efaisa",
-  password: "1q2w3e4r5t6y",
-  database: "interfaz",
-});
+/*const connection = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "efaisa",
+        password: "1q2w3e4r5t6y",
+        database: "interfaz",
+      });
+*/
 
 const getInfoIN = (req: any, res: any) => {
   if (req.query.nroin) {
