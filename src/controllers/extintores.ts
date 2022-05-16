3da6f505e6eb5165f7d77446e8747a2ad0bc4baa
Martín Muñoz <103433945+martinm2001@users.noreply.github.com>
2022-05-16 14:21:22 -0300

    Update extintores.ts

--- src/controllers/extintores.ts
+++ src/controllers/extintores.ts
@@ -1,14 +1,14 @@
-//Conexión con la base de datos via archivo conec
-import mysql from "mysql";
-const connection = mysql.createConnection({
-        host: "127.0.0.1",
-        user: "efaisa",
-        password: "1q2w3e4r5t6y",
-        database: "interfaz",
-      });
 const dayjs = require("dayjs");
 const isBetween = require("dayjs/plugin/isBetween");
 dayjs.extend(isBetween);
+import mysql from "mysql";
+
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "efaisa",
+  password: "1q2w3e4r5t6y",
+  database: "interfaz",
+});
 
 const calcularVencimientoAnoFabricacion = (tipo: any, fecha: any) => {
   if (
