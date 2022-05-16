cededea3e9872554ec02521317c654a4ea353628
Martín Muñoz <103433945+martinm2001@users.noreply.github.com>
2022-05-16 12:07:05 -0300

    Update audioevac.ts

--- src/controllers/audioevac.ts
+++ src/controllers/audioevac.ts
@@ -1,13 +1,11 @@
 //Conexión con la base de datos via archivo conec
 import mysql from "mysql";
-const connection = require("./conec");
-/*const connection = mysql.createConnection({
+const connection = mysql.createConnection({
         host: "127.0.0.1",
         user: "efaisa",
         password: "1q2w3e4r5t6y",
         database: "interfaz",
       });
-*/
 const getAudioEvac = (req: any, res: any) => {
   connection.query(
     "SELECT activado, entrada1, entrada2, tiempoEntradas, tiempoSalida FROM audioevac",
