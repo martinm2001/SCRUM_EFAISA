efdc0c84ba87466e3ad9383546436c595f5f3c65
Scrum <martinalejo.m01@gmail.com>
2022-05-16 16:43:06 -0300

    de

--- src/routes/tecnica.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-const { getTecnica } = require("../controllers/tecnica");
-
-router.route("/").get(getTecnica);
-
-module.exports = router;
