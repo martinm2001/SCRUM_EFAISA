7bb799ea2eca584004fdf19cb63e619b2a7f9891
Martín Muñoz <103433945+martinm2001@users.noreply.github.com>
2022-05-16 09:45:33 -0300

    Update dispositivo.ts

--- src/controllers/dispositivo.ts
+++ src/controllers/dispositivo.ts
@@ -1,4 +1,15 @@
 const dayjs = require("dayjs");
+import mysql from "mysql";
+function conexion(){
+
+    const connection = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "efaisa",
+        password: "1q2w3e4r5t6y",
+        database: "interfaz",
+      });
+      return connection;
+}
 
 let estadosDispositivos : any = [];
 
