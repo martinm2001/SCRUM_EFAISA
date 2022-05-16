293a156be321c0f8ca3314270c5cce3a7b7446ba
Martín Muñoz <103433945+martinm2001@users.noreply.github.com>
2022-05-16 10:43:25 -0300

    Update dispositivo.ts

--- src/controllers/dispositivo.ts
+++ src/controllers/dispositivo.ts
@@ -2,12 +2,14 @@ const dayjs = require("dayjs");
 //Conexión con la base de datos via archivo conec
 import mysql from "mysql";
 
-const connection = mysql.createConnection({
+const connection = require("./conec");
+/*const connection = mysql.createConnection({
         host: "127.0.0.1",
         user: "efaisa",
         password: "1q2w3e4r5t6y",
         database: "interfaz",
       });
+*/
 
 let estadosDispositivos : any = [];
 
